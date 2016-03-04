8b54373ed0110dd41b8cfc7998d3f88543c19f66
justfrankctc <fjuarez@ctconsulting.com.mx>
2016-03-04 15:40:34 -0600

    Final viernes

--- examples/ReciveMessage.php
+++ examples/ReciveMessage.php
@@ -15,32 +15,39 @@
 	require_once __DIR__.'/../src//events/MyEvents.php';
 	
 	//Change the time zone if you are in a different country
-	// Se configura la hora local
 	date_default_timezone_set('America/Mexico_City');
 
 	// Nos conectamos a la red de WhatsApps			
-		$w = new WhatsProt($username, $nickname, $debug);
-		$w->connect(); 
-		$w->loginWithPassword($password); 
+	$w = new WhatsProt($username, $nickname, $debug);
+	$w->connect(); 
+	$w->loginWithPassword($password); 
 		
 	// Enviamos nuestra configuración al servidor
-
-		$w->sendGetServerProperties();
-		$w->sendClientConfig(); 
-		// echo "<br/><div class='dataOutput'>Server config successful</div>";
+	$w->sendGetServerProperties(); // Obtenemos las propiedades delservidor
+	$w->sendClientConfig(); // Enviamos nuestra configuración al servidor
 	
-	$i=0;
+	// Sincronizamos el contacto
+	$sync = [$target];
+	$w->sendSync($sync); 
 
-		$w->pollMessage();
-		$msgs = $w->getMessages();
-		// $pn->process($w);	
-		foreach ($msgs as $m) {
-			$i++;	    	
-	    	$from_number = $m->getAttribute("from");
-		    $from_nickname = $m->getAttribute("notify");
-		    $msg = $m->NodeString();
-		    $messages[$i]=$msg;   
-	    }  
-    
-	$w->disconnect();
+	// inicializamos el apuntador	
+	$i=0;
+	
+	$w->pollMessage();
+	
+	$msgs = $w->getMessages();
+	// $pn->process($w);	
+	
+	foreach ($msgs as $m) {
+		$i++;	    	
+		$from_number = $m->getAttribute("from");
+		$from_nickname = $m->getAttribute("notify");
+		$msg = $m->NodeString();
+		$messages[$i]=$msg;   
+	}
+
+	foreach ($messages as $me) {
+		echo "<hr/>$me";
+	}  
+    $w->disconnect();
 ?>
---
+++ recibeMensajes.js
@@ -0,0 +1,5 @@
+j$= jQuery.noConflict();
+j$(document).ready(function(){
+	// alert('document ready!');
+    j$('#messages').load('http://www.w3schools.com');
+});
